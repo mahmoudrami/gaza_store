c386c9a583302c3685fe296b71bcdae14aef80ce
mahmoudrami <momo.2003mo2@gmail.com>
2024-08-28 10:33:02 +0300

    gaza_store

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,26 +1,24 @@
-import './bootstrap';
+import "./bootstrap";
 
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
 Alpine.start();
 
-
-Echo.private('App.Models.User.' + userId)
-    .notification((notification) => {
-        toastr.success(notification.msg)
-        let count = $('.dropdown .badge-counter').data('count')
-        if(count < 5){
-            $('.dropdown .badge-counter').removeClass('d-none')
-            $('.dropdown .badge-counter').data('count', (count + 1))
-            $('.dropdown .badge-counter').text( count + 1 )
-        }else{
-            $('.dropdown .badge-counter').data('count', (count + 1))
-            $('.dropdown .badge-counter').text('5*')
-        }
+Echo.private("App.Models.User." + userId).notification((notification) => {
+    toastr.success(notification.msg);
+    let count = $(".dropdown .badge-counter").data("count");
+    if (count < 5) {
+        $(".dropdown .badge-counter").removeClass("d-none");
+        $(".dropdown .badge-counter").data("count", count + 1);
+        $(".dropdown .badge-counter").text(count + 1);
+    } else {
+        $(".dropdown .badge-counter").data("count", count + 1);
+        $(".dropdown .badge-counter").text("5*");
+    }
 });
-$('dropdown badge-counter').lenght
+$("dropdown badge-counter").lenght;
 // console.log($('.dropdown .badge-counter').text())
 // console.log($('.dropdown .badge-counter').data('count'))
 // let count = $('.dropdown .badge-counter').data('count')
